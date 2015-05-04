d2e4483f67ff9bea5a5605cf7dc0eb46ca5d31b9
Christoph Molnar <christoph.molnar@googlemail.com>
2015-05-04 20:55:14 +0200

    use short region names

--- projects/winprob/app/forms.py
+++ projects/winprob/app/forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, SelectField
 from wtforms.validators import DataRequired
 
 class SummonerSearchForm(Form):
-    region = SelectField(u'Region', choices=[('euw', 'Europe West'), 
-                                             ('na', 'North America'), 
-                                             ('eune', 'Europe Nordic & East'), 
-                                             ('br', 'Brazil'),
-                                             ('tr', 'Turkey'),
-                                             ('ru', 'Russia'),
-                                             ('lan', 'Latin America North'),
-                                             ('las', 'Latin America South'), 
-                                             ('oce', 'Oceania')
+    region = SelectField(u'Region', choices=[('euw', 'EUW'), 
+                                             ('na', 'NA'), 
+                                             ('eune', 'EUNE'), 
+                                             ('br', 'BR'),
+                                             ('tr', 'TR'),
+                                             ('ru', 'RU'),
+                                             ('lan', 'LAN'),
+                                             ('las', 'LAS'), 
+                                             ('oce', 'OCE')
                                          ])
     summoner = StringField('Summoner Name')
 
