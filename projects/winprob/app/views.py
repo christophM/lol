2f948d429adfd717ef9bfc2fe7b15483d7b485f0
Christoph Molnar <christoph.molnar@googlemail.com>
2015-05-25 09:04:53 +0200

    cache input

--- projects/winprob/app/views.py
+++ projects/winprob/app/views.py
@@ -1,9 +1,12 @@
 from flask import render_template, flash, redirect, request, Flask, flash, url_for
 from forms import SummonerSearchForm
+from flask.ext.cache import Cache
 
 import config
 app = Flask(__name__)
 app.config.from_object("config")
+
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 
 import sys
@@ -28,6 +31,14 @@ def index():
                            form=form)
 
 
+@cache.memoize(timeout=36000)
+def get_summoner_id_cached(region, summoner_name):
+    return winprob.get_summoner_id(region, summoner_name)
+
+@cache.memoize(timeout=1800)
+def get_last_match_cached(region, summonerId):
+    return winprob.get_last_match_id(region, summonerId)
+
 @app.route('/search', methods=('GET', 'POST'))
 def search():
     form = SummonerSearchForm()
@@ -35,12 +46,12 @@ def search():
         summoner_name = form.summoner.data.lower()
         region = form.region.data
         try: 
-            summonerId = winprob.get_summoner_id(region, summoner_name)
+            summonerId = get_summoner_id_cached(region, summoner_name)
         except: 
             flash("Could not find summoner with name %s in region %s"  % (summoner_name, region))
             return redirect(url_for("index"))
         try:
-            matchId = winprob.get_last_match_id(region, summonerId)
+            matchId = get_last_match_cached(region, summonerId)
         except:
             flash("Could not find any ranked matches for summoner %s in region %s" % (summoner_name, region))
             return redirect(url_for("index"))
@@ -55,6 +66,7 @@ def search():
 
 
 @app.route("/match/<region>/<summonerName>/<matchId>")
+@cache.cached(timeout=60)
 def match(region, summonerName, matchId):
     form = SummonerSearchForm()
     last_match = winprob.get_last_match(region=region, matchId=matchId, summonerName=summonerName)    
