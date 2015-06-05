0fc378ee57b77c1b1bf9cde176f58d563d49b037
Christoph Molnar <christoph.molnar@googlemail.com>
2015-06-05 20:49:53 +0200

    add rect highlight and eventbox

--- projects/winprob/app/static/winprob.js
+++ projects/winprob/app/static/winprob.js
@@ -7,7 +7,7 @@ var margin = {
     left: 40
 };
 var width = 720 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+height = 300 - margin.top - margin.bottom;
 
 
 var svg_outer = d3.select("#plot").append("svg")
@@ -15,9 +15,8 @@ var svg_outer = d3.select("#plot").append("svg")
     .attr("height", height + margin.top + margin.bottom);
 
 var svg = svg_outer.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     .attr("class", "winprob");
-
 
 
 var formatAsPercentage = d3.format(".0%");
@@ -30,6 +29,8 @@ var yScale = d3.scale.linear()
     .domain([0, 1])
     .range([height, 0]);
 
+
+
 var lines = svg.selectAll("line")
     .data(winprob_line)
     .enter()
@@ -39,16 +40,51 @@ var lineAttributes = lines.attr("x1", function (d) {
     return xScale(d.timestamp - 1);
 })
     .attr("x2", function (d) {
-    return xScale(d.timestamp);
-})
+        return xScale(d.timestamp);
+    })
     .attr("y1", function (d) {
-    return yScale(d.winprob_before);
-})
+        return yScale(d.winprob_before);
+    })
     .attr("y2", function (d) {
-    return yScale(d.winprob_now);
-})
+        return yScale(d.winprob_now);
+    })
     .style("stroke", "rgb(218,165,32)")
-    .style("stroke-width", "4");
+    .style("stroke-width", "4")
+
+// Create invisible overlay
+var rects = svg.selectAll("rect")
+    .data(winprob_line)
+    .enter().append("svg:rect")
+    .attr("x", function(d) {return xScale(d.timestamp - 1)})
+    .attr("y", yScale(1))
+    .attr("width", xScale(1))
+    .attr("height", height)
+    .attr("opacity", 0)
+    .attr("background", "red")
+
+
+// Tooltip for diyplaying more informations
+var tooltip = svg.append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+var eventbox = d3.select(".eventbox")
+
+var rectsMouse = rects.on("mouseover", function(d) {
+    d3.select(d3.event.target).classed("highlight", true);
+    tooltip.transition()
+        .style("opacity", .9);
+    tooltip.html("Minute : " + d.timestamp + "<br/>"  + d.close)
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    eventbox.html("Minute: " + d.timestamp + "</br> Winchance change from </br>" + Math.round(d.winprob_before * 100) +  "% to " + Math.round(d.winprob_now * 100) + "%")
+ })
+     .on("mouseout", function(d) {
+	 d3.select(d3.event.target).classed("highlight", false);
+        tooltip.transition()
+            .style("opacity", 0);
+    });
+
 
 //Create the Axis
 var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
@@ -73,40 +109,40 @@ var yAxisGroup = svg.append("g")
 svg.selectAll("line.horizontalGrid").data(yScale.ticks(2)).enter()
     .append("line")
     .attr({
-    "class": "horizontalGrid",
+        "class": "horizontalGrid",
         "x1": 0,
         "x2": width,
         "y1": function (d) {
-        return yScale(d);
-    },
+            return yScale(d);
+        },
         "y2": function (d) {
-        return yScale(d);
-    },
+            return yScale(d);
+        },
         "fill": "none",
         "shape-rendering": "crispEdges",
         "stroke": "black",
         "stroke-width": "1px",
         "stroke-dasharray": "2, 2"
-});
+    });
 
 svg.selectAll("line.vertcialGrid").data(xScale.ticks(5)).enter()
     .append("line")
     .attr({
-    "class": "verticalGrid",
+        "class": "verticalGrid",
         "y1": 0,
-"y2": height,
+        "y2": height,
         "x1": function (d) {
-        return xScale(d);
-    },
+            return xScale(d);
+        },
         "x2": function (d) {
-        return xScale(d);
-    },
+            return xScale(d);
+        },
         "fill": "none",
         "shape-rendering": "crispEdges",
         "stroke": "black",
         "stroke-width": "1px",
         "stroke-dasharray": "2, 2"
-});
+    });
 
 // Add plot title
 svg.append("text")
@@ -118,9 +154,9 @@ svg.append("text")
     .text("Win chance during match")
 
 svg.append("text")
-   .attr("x", (width / 2))
-   .attr("y", height + margin.bottom/2)
-   .attr("text-anchor", "middle")
-   .attr("fill", "lightgrey")
-   .style("font-size", "16px")
-   .text("Match minutes");
+    .attr("x", (width / 2))
+    .attr("y", height + margin.bottom/2)
+    .attr("text-anchor", "middle")
+    .attr("fill", "lightgrey")
+    .style("font-size", "16px")
+    .text("Match minutes");
